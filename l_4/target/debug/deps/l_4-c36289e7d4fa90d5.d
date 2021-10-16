f19e391b7fedb797f876ef24f7c30d3387a3963a
Akın Kara <akin.kara@rakort.com>
2021-10-17 00:08:37 +0300

    lesson 4 loops

---
+++ l_4/target/debug/deps/l_4-c36289e7d4fa90d5.d
@@ -0,0 +1,5 @@
+/home/akin/Documents/rust_tutorial/l_4/target/debug/deps/l_4-c36289e7d4fa90d5: src/main.rs
+
+/home/akin/Documents/rust_tutorial/l_4/target/debug/deps/l_4-c36289e7d4fa90d5.d: src/main.rs
+
+src/main.rs:
---
+++ l_4/target/debug/l_4.d
@@ -0,0 +1,1 @@
+/home/akin/Documents/rust_tutorial/l_4/target/debug/l_4: /home/akin/Documents/rust_tutorial/l_4/src/main.rs
